6c1f165c054ac03924bde69b133c8ac7602a79f3
Johs Kristoffersen <johs.kristoffersen@audienceproject.com>
2021-02-02 18:59:18 +0100

    more tests

--- src/test/scala/com/audienceproject/crossbow/core/ConstuctionTest.scala
+++ src/test/scala/com/audienceproject/crossbow/core/ConstuctionTest.scala
@@ -13,5 +13,14 @@ class ConstuctionTest extends AnyFunSuite {
   test("construct using implicits") {
     import com.audienceproject.crossbow.Implicits._
     val df = Seq.empty[(Int,Long)].toDataframe()
+    assertResult(2)(df.numColumns)
+    assertResult(Schema(Seq(Column("_0",expr.IntType),Column("_1",expr.LongType))))(df.schema)
+  }
+
+  test("construct with names") {
+    import com.audienceproject.crossbow.Implicits._
+    val df = Seq.empty[(Int,Long)].toDataframe("a","b")
+    assertResult(2)(df.numColumns)
+    assertResult(Schema(Seq(Column("a",expr.IntType),Column("b",expr.LongType))))(df.schema)
   }
 }
