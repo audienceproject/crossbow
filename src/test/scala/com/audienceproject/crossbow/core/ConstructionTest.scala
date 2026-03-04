9913bba9a0d72d6c605feeec61b45e6ad5854655
Steffen Bach <steffen@audienceproject.com>
2026-03-04 16:28:11 +0100

    Build data frame column by column
    Recent revelations have made it apparent that DataFrame.fromColumns()
    can cause auto-boxing of simple values due to Any in the signature.
    Boxed values are immediately converted back to simple numeric types
    afterwards in convert(). This is obviously a waste of both CPU and
    memory resources, and the 3-4x* memory increase becomes problematic as
    the number of rows increases.
    
    Adding a builder pattern to construct a data frame column by column
    avoids auto-boxing of simple types.
    
    *) Long and Double consume 3x memory when boxed. Int and Float consume
       4x memory when boxed, while a boxed Boolean consumes up to 16x memory
       in the worst case.

--- src/test/scala/com/audienceproject/crossbow/core/ConstructionTest.scala
+++ src/test/scala/com/audienceproject/crossbow/core/ConstructionTest.scala
@@ -25,3 +25,39 @@ class ConstructionTest extends AnyFunSuite:
     val df = Seq.empty[(Int, Float)].toDataFrame("a", "b")
     assertResult(2)(df.numColumns)
     assertResult(Schema(Seq(Column[Int]("a"), Column[Float]("b"))))(df.schema)
+
+  test("Construct using builder supports empty columns"):
+    val expectedSchema = Schema(Seq(Column[Int]("a"), Column[Float]("b"), Column[String]("c"), Column[Integer]("d")))
+
+    val df = DataFrame.builder()
+      .withColumn("a", Array.empty[Int])
+      .withColumn("b", Array.empty[Float])
+      .withColumn("c", Array.empty[String])
+      .withColumn("d", Array.empty[Integer])
+      .toDataFrame
+
+    assertResult(4)(df.numColumns)
+    assertResult(0)(df.rowCount)
+    assertResult(expectedSchema)(df.schema)
+
+  test("Construct using builder produces valid dataframe"):
+    val expectedSchema = Schema(Seq(Column[Int]("a"), Column[Float]("b"), Column[String]("c"), Column[Integer]("d")))
+    val expectedRows = Seq((1, 0.1f, "a", 10), (2, 0.2f, "b", 20), (3, 0.3f, "c", 30))
+
+    val df = DataFrame.builder()
+      .withColumn("a", Array(1, 2, 3))
+      .withColumn("b", Array(0.1f, 0.2f, 0.3f))
+      .withColumn("c", Array("a", "b", "c"))
+      .withColumn("d", Array(10, 20, 30).map(Int.box))
+      .toDataFrame
+
+    assertResult(4)(df.numColumns)
+    assertResult(3)(df.rowCount)
+    assertResult(expectedSchema)(df.schema)
+    assertResult(expectedRows)(df.as[(Int, Float, String, Integer)].toSeq)
+
+  test("Construct using builder prevents jagged dataframe"):
+    val ex = intercept[IllegalArgumentException]:
+      DataFrame.builder().withColumn("a", Array(1, 2, 3)).withColumn("b", Array(0.1f, 0.2f))
+
+    assertResult("Column 'b' must contain 3 rows. Was 2.")(ex.getMessage)
